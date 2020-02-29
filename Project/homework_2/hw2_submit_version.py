856b53480356891fc2bf638fb37d79aae33e23c6
An <atn@iastate.edu>
2020-02-28 22:41:13 -0600

    adding submitted version of the code and report

--- Project/homework_2/hw2_submit_version.py
+++ Project/homework_2/hw2_submit_version.py
@@ -1,6 +1,92 @@
-from homework_2.models import GDAModel, LogisticModel
 import gzip
 import numpy as np
+
+
+class GDAModel:
+
+    def __init__(self, mu0, mu1, phi, covariance):
+        self.mu0 = mu0
+        self.mu1 = mu1
+        self.phi = phi
+        self.cov = covariance
+        self.no_of_feature = mu0.shape[0]
+
+    def predict(self, x):
+
+        y_predict = np.zeros(x.shape[0])
+        for i in range(x.shape[0]):
+            if self.compute_exp_term(x[i, :], self.mu0) < self.compute_exp_term(x[i, :], self.mu1):
+                y_predict[i] = 1
+
+        return y_predict
+
+    def compute_exp_term(self, x, mu):
+        return -1 / 2 * np.dot(np.dot(np.transpose(x - mu), np.linalg.inv(self.cov)), (x - mu))
+
+    @staticmethod
+    def compute_variance(x, y, mu0, mu1):
+
+        if y.shape[0] > 0:
+            var = (np.sum((x[np.where(y == 0)[0]] - mu0) ** 2) + np.sum((x[np.where(y == 1)[0]] - mu1) ** 2))/y.shape[0]
+            return var
+        else:
+            return None
+
+    @staticmethod
+    def gda_estimate(x, y):
+
+        if y.shape[0] == 0:
+            return GDAModel(None, None, None)
+        else:
+            estimate_phi = np.sum(y) / y.shape[0]
+
+        if np.sum(y) < y.shape[0]:  # will only work if y is Bernoulli distributed
+            estimate_mu0 = np.sum(x[np.where(y == 0)[0], :], axis=0) / np.where(y == 0)[0].shape[0]
+        else:
+            estimate_mu0 = None
+
+        if np.sum(y) > 0:
+            estimate_mu1 = np.sum(x[np.where(y == 1)[0], :], axis=0) / np.where(y == 0)[0].shape[0]
+        else:
+            estimate_mu1 = None
+
+        # initialize covariance matrix with zeroes entries of dimension n x n
+        estimate_cov = np.zeros(x.shape[1] ** 2).reshape(x.shape[1], x.shape[1])
+        for i in range(x.shape[1]):
+            estimate_cov[i, i] = GDAModel.compute_variance(x[:, i], y, estimate_mu0[i], estimate_mu1[i])
+        model = GDAModel(estimate_mu0, estimate_mu1, estimate_phi, estimate_cov)
+
+        return model
+
+
+class LogisticModel:
+
+    def __init__(self, parameters):
+        self.parameters = parameters
+
+    @staticmethod
+    def logistic_estimate(x, y, max_iter):
+        learn_rate = (1e-2) / y.shape[0]
+        theta_hat = np.ones(x.shape[1])
+
+        for t in range(max_iter):
+            hx = (1 / (1 + np.exp(-np.dot(x, theta_hat))))
+            theta_hat = theta_hat - learn_rate * (np.dot(np.transpose(x), (hx - y)))
+
+        model = LogisticModel(theta_hat)
+        return model
+
+    def predict(self, x):
+
+        hx = (1 / (1 + np.exp(-np.dot(x, self.parameters))))
+        y_hat = np.zeros(hx.shape[0])
+
+        for i in range(0, len(hx)):
+            if hx[i] >= 0.5:
+                y_hat[i] = 1
+            else:
+                y_hat[i] = 0
+        return y_hat
 
 
 class MnistFileInfo:
@@ -83,20 +169,6 @@ def compute_accuracy(y, y_predict):
     return accuracy
 
 
-def normalize_all_features(X):
-
-    mu = np.sum(X, axis=0) / X.shape[0]  # means of each column
-
-    shifted_x = np.subtract(X, mu)  # shift location of x
-    covariance = np.dot(np.transpose(shifted_x), shifted_x)
-    sd_vector = np.sqrt(np.diag(covariance))  # standard deviation of each column
-    sd_vector = np.where(sd_vector == 0, 1, sd_vector)  # for columns with 0 variance
-
-    scaled_x = np.dot(shifted_x, np.diag(np.reciprocal(sd_vector.astype(float))))
-
-    return scaled_x
-
-
 def main():
 
     files = {
@@ -136,19 +208,15 @@ def main():
 
         # Applying GDA Model
         # remove all zeros columns in images files
-        gda_train_images, removed_cols = remove_zero_columns(train_images)
-        gda_test_images = np.delete(test_images, removed_cols, 1)
+        train_images, removed_cols = remove_zero_columns(train_images)
+        test_images = np.delete(test_images, removed_cols, 1)
 
         train_labels = np.where(train_labels == 9, 1, 0)  # since the coded models only work for levels 0, 1
-        gda_model = GDAModel.gda_estimate(gda_train_images, train_labels)  # estimate
-        gda_predict_labels = np.where(gda_model.predict(gda_test_images) == 1, 9, 0)  # predict
+        gda_model = GDAModel.gda_estimate(train_images, train_labels)  # estimate
+        gda_predict_labels = np.where(gda_model.predict(test_images) == 1, 9, 0)  # predict
         gda_accuracy = compute_accuracy(test_labels, gda_predict_labels)  # compute accuracy
 
         # Applying the Logistic Regression Model
-        # Normalizing the images
-        train_images = normalize_all_features(train_images)
-        test_images = normalize_all_features(test_images)
-
         logistic_model = LogisticModel.logistic_estimate(train_images, train_labels, max_iter=1000)
         lr_predict = np.where(logistic_model.predict(test_images) == 1, 9, 0)
         lr_accuracy = compute_accuracy(test_labels, lr_predict)
